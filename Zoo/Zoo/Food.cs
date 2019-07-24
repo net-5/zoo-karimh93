08c187b44f17a2e819b4f93abb3e65ed873796d5
karimh93 <karim.husein0293@gmail.com>
2019-07-24 13:58:39 +0300

    First Commit

---
+++ Zoo/Zoo/Food.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Food : Animals
+    {
+
+        public Food(animaltype animalType) : base(animalType)
+        {
+            this.AnimalType = animalType;
+
+
+        }
+
+        public void Eat()
+        {
+            if (AnimalType == animaltype.lion)
+            {
+                Console.WriteLine("Lion eats meat");
+            }
+            if (AnimalType == animaltype.elephant)
+            {
+                Console.WriteLine("Elephant eats grass");
+            }
+            if (AnimalType == animaltype.giraffe)
+            {
+                Console.WriteLine("Giraffe eats leaves");
+            }
+            if (AnimalType == animaltype.rabbit)
+            {
+                Console.WriteLine("Rabbit eats carrots");
+            }
+            if (AnimalType == animaltype.horse)
+            {
+                Console.WriteLine("Horse eats apples");
+            }
+        }
+
+
+
+    }
+}
---
+++ Zoo/Zoo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Food food = new Food(animaltype.elephant);
+            Actions first = new Actions("Iasi");
+            Actions second = new Actions("Bucharest");
+
+            Animals firstAnimal = new Animals(animaltype.horse);
+            Animals secondAnimal = new Animals(animaltype.giraffe);
+
+            
+
+            first.TransferAnimal(first, secondAnimal);
+
+
+
+
+        }
+    }
+}
